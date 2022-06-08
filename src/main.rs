ab37bbac60a92cec0f4ecdd0d0d697f6f592896b
CunningBard <jhonjhonjumaday@gmail.com>
2022-06-09 00:23:51 +0800

    added basic grid

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,81 @@
+mod essential_functions;
+
 use ggez::{Context, ContextBuilder, GameResult};
 use ggez::graphics::{self, Color};
 use ggez::event::{self, EventHandler};
 
+#[derive(PartialEq, Debug)]
+struct Position
+{
+    x: i32,
+    y: i32
+}
 
+impl Position
+{
+    fn position(x: i32, y: i32) -> Position{
+        Position{
+            x,
+            y
+        }
+    }
+}
+
+#[derive(PartialEq, Debug)]
 struct BurnablePoint
 {
     is_burning: bool,
-    burning_level: i8
+    burning_level: i8,
+    position: Position
 }
 
+impl BurnablePoint
+{
+    fn burnable_point(position: Position )-> BurnablePoint{
+        BurnablePoint {
+            is_burning: false,
+            burning_level: 0,
+            position
+        }
+    }
+}
+
+#[derive(Debug)]
 struct Grid
 {
-    grid: Vec<Vec<BurnablePoint>>
+    size_x: i32,
+    size_y: i32,
+    grid: Vec<Vec<BurnablePoint>>,
+    burning_positions: Vec<Position>
 }
 
-fn main() {
-    // Make a Context.
+impl Grid {
+    fn grid(size_x: i32, size_y: i32) -> Grid{
+        let mut grid = vec![];
+        for y in 0..size_y {
+            let mut y_axis = vec![];
+            for x in 0..size_x {
+                y_axis.push(BurnablePoint::burnable_point(Position::position(x, y)))
+            }
+            grid.push(y_axis);
+        }
+        Grid {
+            grid,
+            size_x,
+            size_y,
+            burning_positions: vec![]
+        }
+    }
+    fn random_burn(&mut self){
+        let
+    }
+}
+
+fn main() { // Make a Context.
+    let d = Grid::grid(100, 100);
+
+
+
     let (mut ctx, event_loop) = ContextBuilder::new("my_game", "Cool Game Author")
         .build()
         .expect("aieee, could not create ggez context!");
