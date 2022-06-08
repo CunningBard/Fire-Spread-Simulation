e91f060f5d4e38838be6b449a04e08f862076f41
CunningBard <jhonjhonjumaday@gmail.com>
2022-06-08 23:58:12 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+use ggez::{Context, ContextBuilder, GameResult};
+use ggez::graphics::{self, Color};
+use ggez::event::{self, EventHandler};
+
+
+struct BurnablePoint
+{
+    is_burning: bool,
+    burning_level: i8
+}
+
+struct Grid
+{
+    grid: Vec<Vec<BurnablePoint>>
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Make a Context.
+    let (mut ctx, event_loop) = ContextBuilder::new("my_game", "Cool Game Author")
+        .build()
+        .expect("aieee, could not create ggez context!");
+
+    // Create an instance of your event handler.
+    // Usually, you should provide it with the Context object to
+    // use when setting your game up.
+    let my_game = MyGame::new(&mut ctx);
+
+    // Run!
+    event::run(ctx, event_loop, my_game);
+}
+
+struct MyGame {
+    // Your state here...
+}
+
+impl MyGame {
+    pub fn new(_ctx: &mut Context) -> MyGame {
+        // Load/create resources such as images here.
+        MyGame {
+            // ...
+        }
+    }
+}
+
+impl EventHandler for MyGame {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        // Update code here...
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx, Color::WHITE);
+        // Draw code here...
+        graphics::present(ctx)
+    }
 }
