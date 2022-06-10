f2933187eebc76687f8876af07031ab2f9d29fc7
Eric <jhonjhonjumaday@gmail.com>
2022-06-10 11:46:04 +0800

    added switch bool and reorganize

--- src/essential_functions.rs
+++ src/essential_functions.rs
@@ -2,16 +2,13 @@ use rand::thread_rng;
 use rand::Rng;
 use rand::seq::SliceRandom;
 
-pub fn rand_prob_(number: i32, to: i32) -> bool
-{
-    let num = thread_rng().gen_range(0..to);
-    number > num
-}
+
 
 pub fn rand_prob(number: i32) -> bool
 {
     rand_prob_(number, 100)
 }
+
 pub fn rand_range(start: i32, stop: i32) -> i32
 {
     thread_rng().gen_range(start..stop)
@@ -23,6 +20,14 @@ pub fn vec_shuffle<T>(vec: &mut Vec<T>)
 }
 
 pub fn rand_item_index<T>(vec: Vec<T>) -> usize {rand_range(0, vec.len() as i32) as usize}
+
+fn switch_bool(boolean: bool) -> bool { !boolean }
+
+pub fn rand_prob_(number: i32, to: i32) -> bool
+{
+    let num = thread_rng().gen_range(0..to);
+    number > num
+}
 
 pub fn rand_number_increase_prob(mut start_prob: i32, minus_per_iteration: i32) -> i32
 {
